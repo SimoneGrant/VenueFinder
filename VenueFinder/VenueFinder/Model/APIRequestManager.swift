11bf47b13af30ff852763e55fb7646da2d6233cd
SimoneGrant <simonegrant001@gmail.com>
2017-11-10 15:15:35 -0500

    uploading base files

---
+++ VenueFinder/VenueFinder/Model/APIRequestManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIRequestManager.swift
+//  VenueFinder
+//
+//  Created by Simone Grant on 11/10/17.
+//  Copyright © 2017 Simone Grant. All rights reserved.
+//
+
+import Foundation
---
+++ VenueFinder/VenueFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VenueFinder
+//
+//  Created by Simone Grant on 11/10/17.
+//  Copyright © 2017 Simone Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
