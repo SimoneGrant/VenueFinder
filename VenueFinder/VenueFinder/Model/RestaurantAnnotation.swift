69fd532f4e170b5b9a15418a1fbf5ca869e14e6f
SimoneGrant <simonegrant001@gmail.com>
2017-11-10 15:27:22 -0500

    Venue Annotation

---
+++ VenueFinder/VenueFinder/Model/RestaurantAnnotation.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantAnnotation.swift
+//  VenueFinder
+//
+//  Created by Simone Grant on 11/10/17.
+//  Copyright © 2017 Simone Grant. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class RestaurantAnnotation: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+}
